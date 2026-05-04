3140414b6056dc3c696be534a6abd7877ba782a7
avdata99 <andres@data99.com.ar>
2026-05-04 13:47:05 -0300

    drop setup test fn

--- ckanext/ddi/tests/test_blueprints.py
+++ ckanext/ddi/tests/test_blueprints.py
@@ -1,4 +1,5 @@
 import os
+from types import SimpleNamespace
 import pytest
 import ckan.plugins.toolkit as toolkit
 from ckantoolkit.tests import factories
@@ -25,22 +26,28 @@ def _patch_storage_path(monkeypatch, tmpdir, ckan_config):
     monkeypatch.setitem(ckan_config, u'ckan.storage_path', str(tmpdir))
 
 
+@pytest.fixture
+def setup_data():
+    """test setup data"""
+    obj = SimpleNamespace()
+    # Create CKAN users
+    obj.sysadmin = factories.SysadminWithToken()
+    return obj
+
+
 @pytest.mark.usefixtures('clean_db', 'clean_index')
 @pytest.mark.ckan_config("ckan.webassets.path", "/tmp/webassets")
 class TestBlueprints:
-    def setup_method(self):
-        sysadmin = factories.SysadminWithToken()
-        self.headers = {'Authorization': sysadmin['token']}
 
     def test_form_display_unauthorized_user(self, app):
         app.get('/dataset/import', status=403)
         app.post('/dataset/import', status=403)
 
-    def test_form_display(self, app, monkeypatch, tmpdir, ckan_config):
+    def test_form_display(self, app, monkeypatch, tmpdir, ckan_config, setup_data):
         _patch_storage_path(monkeypatch, tmpdir, ckan_config)
         resp = app.get(
             '/dataset/import',
-            headers=self.headers,
+            headers={'Authorization': setup_data.sysadmin['token']},
             status=200,
         )
         _assert_in_body('<input id="field-upload" type="file" name="upload"', resp)
@@ -49,12 +56,13 @@ class TestBlueprints:
         )
 
     def test_form_submit_success_xml_file_from_upload(
-        self, app, monkeypatch, tmpdir, ckan_config
+        self, app, monkeypatch, tmpdir, ckan_config, setup_data
     ):
         _patch_storage_path(monkeypatch, tmpdir, ckan_config)
         files = {'upload': 'ddi_test.xml'}
+        headers = {'Authorization': setup_data.sysadmin['token']}
         resp = _post_request(
-            app, '/dataset/import', {}, files, self.headers, status=302
+            app, '/dataset/import', {}, files, headers, status=302
         )
         expected_id = 'ddi-test-1'
         assert (
@@ -69,12 +77,13 @@ class TestBlueprints:
         assert 'ddi_test.xml' in dataset['resources'][0]['url']
 
     def test_form_submit_success_xml_and_rdf_files_from_uploads(
-        self, app, monkeypatch, tmpdir, ckan_config
+        self, app, monkeypatch, tmpdir, ckan_config, setup_data
     ):
         _patch_storage_path(monkeypatch, tmpdir, ckan_config)
         files = {'upload': 'ddi_test.xml', 'rdf_upload': 'ddi_test.rdf'}
+        headers = {'Authorization': setup_data.sysadmin['token']}
         resp = _post_request(
-            app, '/dataset/import', {}, files, self.headers, status=302
+            app, '/dataset/import', {}, files, headers, status=302
         )
         expected_id = 'ddi-test-1'
         assert (
@@ -103,20 +112,22 @@ class TestBlueprints:
         # TODO: test importing from URLs, mock the HTTP requests with responses
     """
 
-    def test_duplicate_dataset(self, app, monkeypatch, tmpdir, ckan_config):
+    def test_duplicate_dataset(self, app, monkeypatch, tmpdir, ckan_config, setup_data):
         _patch_storage_path(monkeypatch, tmpdir, ckan_config)
         files = {'upload': 'ddi_test.xml'}
+        headers = {'Authorization': setup_data.sysadmin['token']}
         resp = _post_request(
-            app, '/dataset/import', {}, files, self.headers, status=302
+            app, '/dataset/import', {}, files, headers, status=302
         )
         resp = _post_request(
-            app, '/dataset/import', {}, files, self.headers, status=200
+            app, '/dataset/import', {}, files, headers, status=200
         )
         _assert_in_body('Dataset already exists and duplicates are not allowed', resp)
 
-    def test_form_submit_invalid(self, app, monkeypatch, tmpdir, ckan_config):
+    def test_form_submit_invalid(self, app, monkeypatch, tmpdir, ckan_config, setup_data):
         _patch_storage_path(monkeypatch, tmpdir, ckan_config)
+        headers = {'Authorization': setup_data.sysadmin['token']}
         resp = _post_request(
-            app, '/dataset/import', {}, {}, self.headers, status=200
+            app, '/dataset/import', {}, {}, headers, status=200
         )
         _assert_in_body('An XML file (uploaded file or URL) is required', resp)
